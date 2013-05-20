b46279e49ca52456613b378b8f5fe5baad369cc6
Despo Pentara <despo@extractmethod.com>
2013-05-20 13:55:29 +0100

    rename method to find_messages instead of find_conversations
    Oh Skype, oh skype

--- lib/find_in_skype/db.rb
+++ lib/find_in_skype/db.rb
@@ -9,7 +9,7 @@ module FindInSkype
       @database.execute("SELECT DISTINCT(dialog_partner) FROM Messages")
     end
 
-    def find_conversation_with user
+    def find_messages_between user
       @database.execute("SELECT author, from_dispname, datetime(timestamp, 'unixepoch') as date, body_xml FROM Messages where dialog_partner = '#{user}' ORDER BY timestamp")
     end
   end
--- spec/find_in_skype_spec.rb
+++ spec/find_in_skype_spec.rb
@@ -4,12 +4,12 @@ describe FindInSkype do
 
   let(:db) { FindInSkype.configure }
 
-  it "retrieve all users that I had conversations with" do
+  it "retrieve all users that I exchanged messages with" do
     db.find_contacts.should_not be_empty
     db.find_contacts.should be_an Array
   end
 
-  it 'retrieves a conversation by username' do
-    db.find_conversation_with("find_in_skype").flatten.should include "find_in_skype", "hey ho"
+  it 'retrieves all messages of a discussion' do
+    db.find_messages_between("find_in_skype").flatten.should include "find_in_skype", "hey ho"
   end
 end
