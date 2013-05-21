8c087e6d0c50cb4985db3388ee34e94a1142d9dd
Despo Pentara <despo@extractmethod.com>
2013-05-21 14:34:53 +0100

    update specs to use sample database

--- spec/find_in_skype_spec.rb
+++ spec/find_in_skype_spec.rb
@@ -2,29 +2,31 @@ require 'spec_helper'
 
 describe FindInSkype do
 
-  let(:db) { FindInSkype.configure }
+  subject(:db) { FindInSkype::DB.new("#{Dir.pwd}/spec/data/sample.db") }
 
   it "retrieve all users that I exchanged messages with" do
-    db.find_contacts.should_not be_empty
+    db.find_contacts.should have(1).items
   end
 
   it 'retrieves all messages of a discussion' do
     db.find_messages_between("find_in_skype").last.body_xml.should include "hey ho"
   end
 
-  it 'retrieves all conversations' do
-    conversations =  db.find_conversations
-    conversations.should_not be_empty
+  it 'retrieves all conversation details' do
+    conversations = db.find_conversations
+    conversations.should have(1).items
   end
 
   it 'retrieves a conversation' do
     conversation_details = db.find_conversations.last
+
     conversation = db.find_conversation_by_id conversation_details
     conversation.first.convo_id.should eq conversation_details.id
   end
 
   it 'searches all conversations for a given text' do
     conversations = db.search_for "Resistance is futile"
+
     conversations.length.should == 2
   end
 end
